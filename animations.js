62dbea31022575e42449484a70d6a0f3a96f0bed
Will Ballard <wballard@mailframe.net>
2014-12-11 07:32:40 -0600

    less visual flash

--- animations.js
+++ animations.js
@@ -11,9 +11,9 @@ the animation is complete.
 if (!Element.prototype.fadeOut) {
   Element.prototype.fadeOut = function(callback){
     var anim = this.animate([
-          {opacity: 1, transform: 'scale(1)', offset: 0},
-          {opacity: 0, transform: 'scale(.98)', offset: 1}
-          ], {duration: 100, easing: "0.1s cubic-bezier(0.4, 0.0, 1, 1)"});
+          {opacity: 1, transform: 'scale(1, 1)', offset: 0},
+          {opacity: 0, transform: 'scale(.98, .98)', offset: 1}
+          ], {duration: 200, easing: "0.2s cubic-bezier(0.4, 0.0, 1, 1)"});
     anim.onfinish = function () {
       if (callback) {
         callback();
@@ -25,9 +25,23 @@ if (!Element.prototype.fadeOut) {
 if (!Element.prototype.fadeIn) {
   Element.prototype.fadeIn = function(callback){
     var anim = this.animate([
-          {opacity: 0, transform: 'scale(.98)', offset: 0},
-          {opacity: 1, transform: 'scale(1)', offset: 1}
-          ], {duration: 100, easing: "0.1s cubic-bezier(0.4, 0.0, 1, 1)"});
+          {opacity: 0, transform: 'scale(.98, .98)', offset: 0},
+          {opacity: 1, transform: 'scale(1, 1)', offset: 1}
+          ], {duration: 200, easing: "0.2s cubic-bezier(0.4, 0.0, 1, 1)"});
+    anim.onfinish = function () {
+      if (callback) {
+        callback();
+      }
+    }
+  }
+}
+
+if (!Element.prototype.zoomIn) {
+  Element.prototype.zoomIn = function(callback){
+    var anim = this.animate([
+          {opacity: 0.66, transform: 'scale(1.2, 1.2)', 'text-shadow': '0 0 1em', offset: 0},
+          {opacity: 0.66, transform: 'scale(1, 1)', 'text-shadow': '0 0 0', offset: 1}
+          ], {duration: 200, easing: "0.2s cubic-bezier(0.4, 0.0, 1, 1)"});
     anim.onfinish = function () {
       if (callback) {
         callback();
