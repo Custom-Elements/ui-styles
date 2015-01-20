a45bb8140bbfe00dc171029e2f858243bafe859b
Will Ballard <wballard@mailframe.net>
2015-01-20 15:21:03 -0600

    slide in animations

--- animations.js
+++ animations.js
@@ -47,3 +47,59 @@ Element.prototype.zoomIn = function(callback){
     }
   }
 }
+
+Element.prototype.vanishLeft = function(callback){
+  var elem = this;
+  var anim = this.animate([
+    {opacity: 0.66, transform: 'translateX(0)', offset: 0},
+    {opacity: 0.66, transform: 'translateX(-100%)', offset: 1}
+  ], {duration: 200, easing: "0.2s cubic-bezier(0.4, 0.0, 1, 1)"});
+  anim.onfinish = function () {
+    elem.setAttribute('hidden', '');
+    if (callback) {
+      callback();
+    }
+  }
+}
+
+Element.prototype.vanishRight = function(callback){
+  var elem = this;
+  var anim = this.animate([
+    {opacity: 0.66, transform: 'translateX(0)', offset: 0},
+    {opacity: 0.66, transform: 'translateX(100%)', offset: 1}
+  ], {duration: 200, easing: "0.2s cubic-bezier(0.4, 0.0, 1, 1)"});
+  anim.onfinish = function () {
+    elem.setAttribute('hidden', '');
+    if (callback) {
+      callback();
+    }
+  }
+}
+
+Element.prototype.appearRight = function(callback){
+  var elem = this;
+  elem.removeAttribute('hidden');
+  var anim = this.animate([
+    {opacity: 0.66, transform: 'translateX(100%)', offset: 0},
+    {opacity: 0.66, transform: 'translateX(0)', offset: 1}
+  ], {duration: 200, easing: "0.2s cubic-bezier(0.4, 0.0, 1, 1)"});
+  anim.onfinish = function () {
+    if (callback) {
+      callback();
+    }
+  }
+}
+
+Element.prototype.appearLeft = function(callback){
+  var elem = this;
+  elem.removeAttribute('hidden');
+  var anim = this.animate([
+    {opacity: 0.66, transform: 'translateX(-100%)', offset: 0},
+    {opacity: 0.66, transform: 'translateX(0)', offset: 1}
+  ], {duration: 200, easing: "0.2s cubic-bezier(0.4, 0.0, 1, 1)"});
+  anim.onfinish = function () {
+    if (callback) {
+      callback();
+    }
+  }
+}
